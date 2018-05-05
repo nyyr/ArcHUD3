daa435075f4c5608f2b016e400809a1a935f7e56
nyyr <nyyyrikki@gmail.com>
2018-05-05 17:56:23 +0200

    integrated #17 ComboPoints counter for DK and DH by Garrett128

--- ComboPoints.lua
+++ ComboPoints.lua
@@ -1,21 +1,44 @@
 --
--- Show combo points / holy power / soul shards
+-- Show combo points / holy power / soul shards / ...
 --
 
 local _
 local class = ""
 
 function ArcHUD:InitComboPointsFrame()
-	 _, class = UnitClass("player")
+	-- NOTE: Get Player's Class
+	_, class = UnitClass("player")
 	
-	if ((class == "ROGUE") or (class == "DRUID")) then
-		self:RegisterEvent("UNIT_POWER_FREQUENT", "UpdateComboPoints", "player");
-		self:UpdateComboPointsFrame()
+	-- NOTE: Demon Hunter Events
+	if class == "DEMONHUNTER" then 
+		self:RegisterEvent("UNIT_AURA", "UpdateComboPoints");
+	end	
+	
+	-- NOTE: Deathknight Events
+	if class == "DEATHKNIGHT" then 
+		self:RegisterEvent("RUNE_POWER_UPDATE", "UpdateComboPoints");
+		self:RegisterEvent("RUNE_TYPE_UPDATE", "UpdateComboPoints");
 	end
+	
+	-- NOTE: General Events
+	
+	-- NOTE: Unit Power
+	self:RegisterEvent("UNIT_POWER", "UpdateComboPoints")
+	self:RegisterEvent("UNIT_POWER_FREQUENT", "UpdateComboPoints")
+
+	-- NOTE: Entering/Leaving Combat
+	self:RegisterEvent("PLAYER_REGEN_ENABLED", "UpdateComboPoints")
+	-- self:RegisterEvent("PLAYER_REGEN_DISABLED", "UpdateComboPoints")
+	
+	-- NOTE: Dying/Ressurecting/Insance Zoning
+	self:RegisterEvent("PLAYER_ENTERING_WORLD", "UpdateComboPoints")
+	self:RegisterEvent("PLAYER_DEAD", "UpdateComboPoints")
+	self:RegisterEvent("PLAYER_UNGHOST", "UpdateComboPoints")
+	self:RegisterEvent("PLAYER_ALIVE", "UpdateComboPoints")
 	
 	self.TargetHUD.Combo:SetTextColor(self.db.profile.ColorComboPoints.r, self.db.profile.ColorComboPoints.g, self.db.profile.ColorComboPoints.b)
 	
-	-- Show/Hide combopoints display
+	-- NOTE: Show/Hide combopoints display
 	if(self.db.profile.ShowComboPoints) then
 		self.TargetHUD.Combo:Show()
 	else
@@ -34,12 +57,22 @@ end
 function ArcHUD:UpdateComboPointsFrame()
 	local points = 0
 	local _, class = UnitClass("player")
+	
 	if (class == "PALADIN") then
 		points = UnitPower("player", SPELL_POWER_HOLY_POWER)
 	elseif (class == "WARLOCK") then
 		points = UnitPower("player", SPELL_POWER_SOUL_SHARDS)
 	elseif (class == "MONK") then
 		points = UnitPower("player", SPELL_POWER_CHI)
+	elseif (class == "DEATHKNIGHT") then
+		points = self:get_active_runes()
+	elseif (class == "DEMONHUNTER") then		
+		local _, _, _, count = UnitAura("player", "Soul Fragments")
+		if count then 
+			points = count
+		else 
+			points = 0
+		end
 	elseif (class == "ROGUE" or class == "DRUID") then
 		local powerType, powerToken = UnitPowerType("player");
 		if (powerType == SPELL_POWER_ENERGY) then
@@ -49,13 +82,17 @@ function ArcHUD:UpdateComboPointsFrame()
 	self:SetComboPoints(points)
 end
 
-function ArcHUD:UpdateComboPoints(event, arg1, arg2)
-	--self:LevelDebug(3, "UpdateComboPoints("..tostring(event)..", "..tostring(arg1)..")")
-	if (event == "UNIT_POWER_FREQUENT") then
-		if (arg1 == "player" and arg2 == "COMBO_POINTS") then
-			self:UpdateComboPointsFrame()
+function ArcHUD:get_active_runes()
+	local runecount = 0;
+	for runeslot=1,6 do
+		local _, _, runeready = GetRuneCooldown(runeslot);
+		if (runeready) then
+			runecount = runecount + 1;
 		end
-	else
-		self:UpdateComboPointsFrame()
 	end
+	return runecount
+end
+
+function ArcHUD:UpdateComboPoints(event, arg1, arg2)
+	self:UpdateComboPointsFrame()
 end
